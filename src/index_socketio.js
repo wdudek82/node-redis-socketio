25099191caf4e65f5c5205240a7d8564cdc9d4d5
Wojciech Dudek <wdudek82@gmail.com>
2017-07-16 00:06:22 +0200

    Experimenting with socket.io v1.5.1

--- src/index_socketio.js
+++ src/index_socketio.js
@@ -1,3 +1,4 @@
+// !!! its for old version of Socket.io
 const express = require("express");
 const socketio = require("socket.io");
 
@@ -8,7 +9,7 @@ let io = socketio(server);
 
 app.use(express.static("static"));
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
     console.log("A socket is now open");
     console.log(socket);
 });
